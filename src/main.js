a9002d5901511f61fc36b460a35900e37512a94b
yang <1820618325@qq.com>
2022-06-26 16:17:51 +0800

    创建项目

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
-createApp(App).use(router).mount('#app')
+import '../src/style/headmap.css'
+import '../public/reset.css'
+
+
+const app = createApp(App)
+
+app.use(router)
+app.use(ElementPlus)
+
+app.mount('#app')
---
+++ src/views/MainShow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <p>hello word</p>
+  </div>
+</template>
+
+<script>
+export default {
+  setup () {
+
+
+    return {
+    }
+  }
+}
+</script>
+
+<style  >
+
+</style>
